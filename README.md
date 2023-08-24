3cbb97f27fd2727b98da287f2a20400564f1be05
Antonio Campos Serna <campos_antser@gva.es>
2023-08-24 13:26:44 +0200

    [ACS] Versión 0.0.1

--- README.md
+++ README.md
@@ -1,7 +1,55 @@
-# Bricks Game
+# Juego de ladrillos
 
-A simple Bricks Game with javascript
+Versión simple del juego de los ladrillos con javascript.
 
+> Este juego se ha diseñado con propositos de aprenizaje, no está optimizado para un uso profesional.
 
-## License
-[MIT](LICENSE)
+## Instalación
+
+Para ejecutar el juego sólo es necesario un navegador web.
+
+Descargad todos los ficheros en una carpeta de vuestro disco duro y abrir el fichero `index.html` en un navegador web (prefeiblemente Chrome).
+## Instrucciones
+
+El objetivo del juego es destruir todos los ladrillos con la pelota.
+
+Si la pelota sale por el borde inferior de la pantalla, sin que sea devuelta por la requeta, se pierde el juego.
+
+Las teclas de movimiento de la raqueta son las flecas derecha (&rarr;) e izquierda (&larr;).
+
+## Mejoras
+
+* El jugador tendrá tres intentos (vidas) para terminar.
+* Cuando se destruyen todos los ladrillos se cambia de nivel:
+  * Se pintan de nuevo los ladrillos.
+  * Se mantien la puntuación.
+  * Aumenta la dificultad (mayor velocidad de la pelota).
+* Nuevo algorito de colisión entre la pelota y los ladrillos.
+* Almacenar y mostrar el máximo absoluto de puntuación (record).
+* Adaptación a dispositivos móviles.
+
+## Notas
+
+La fuente *Retro Gaming* ha sido diseñada por el usuario [Daymarius](https://www.dafont.com/es/daymarius.d7345)  y se ha obtenido del repositorio [dafont.com](https://www.dafont.com/es/retro-gaming.font)
+
+## MIT License
+
+Copyright (c) 2023 Antonio Campos Serna
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- src/bricks_game.js
+++ src/bricks_game.js
@@ -1,88 +1,321 @@
-// Get the canvas context
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
-
-// Ball
-var ballRadius = 10;
-var x = canvas.width/2;
-var y = canvas.height/2;
-var dx = 2;
-var dy = -2;
-var ballColor = "#0095DD";
-
-// Paddle
-var paddleHeight = 10;
-var paddleWidth = 75;
-var paddleX = (canvas.width - paddleWidth) / 2;
-var paddleDx = 7;
-var paddleColor = "#DD9500";
-
-// Paddle movement
-var rightPressed = false;
-var leftPressed = false;
-var leftKeyCode = 37;
-var rightKeyCode = 39;
-
 /**
- * Draw the ball at (x,y) coordinates.
+ * Juego de los ladrillos
  *
- * @param int x
- * @param int y
+ * Este juego se ha diseñado con propositos de aprenizaje, no está optimizado para un uso profesional.
+ *
+ * @type {HTMLElement}
+ * @author Antonio Campos Serna (https://github.com/acamposserna)
+ * @version 0.0.1
+ * @since 23/08/2023
  */
-function drawBall(x, y) {
+
+/**
+ * CONSTANTES
+ */
+
+// Parámetros de la pelota
+const radioPelota = 10;
+const colorPelota = "#0095DD";
+const velocidadMinima = 2;
+const velocidadMaxima = 6;
+
+// Parámetros de la raqueta
+const altoRaqueta = 10;
+const anchoRaqueta = 75;
+const colorRaqueta = "#DD9500";
+const velocidadRaquetaX = 7;  // La raqueta sólo se mueve en el eje X
+
+// Parámetros de los ladrilos
+const numFilas = 3;
+const numColumnas = 5;
+const altoLadrillo = 20;
+const anchoLadrillo = 75;
+const paddingLadrillo = 10;
+const offsetTopLadrillo = 30;
+const offsetIzqLadrillo = 30;
+const colorLadrillo = "#720004";
+const totalLadrillos = numFilas * numColumnas;
+
+// Parametros de la puntuacion
+const puntuacionX = 10;
+const puntuacionY = 20;
+const colorTexto = "#0095DD";
+
+// Códigos de teclas
+const leftKeyCode = 37;
+const rightKeyCode = 39;
+
+// Intervalo de ejecución de la rutina principal del juego
+const ms = 20;
+
+/**
+ * VARIABLES
+ */
+
+// Pantalla del juego
+var pantalla = document.getElementById("pantalla");
+var ctx = pantalla.getContext("2d");
+
+// Posición de la pelota
+var pelotaX = pantalla.width / 2;
+var pelotaY = pantalla.height - (altoRaqueta + radioPelota); // La posición inicial es en el centro de la raqueta
+
+// Velocidad de la pelota
+var velocidadPelotaX = 0;
+var velocidadPelotaY = 0;
+
+// Posición de la raqueta
+var raquetaX = (pantalla.width - anchoRaqueta) / 2; // La posición inicial es en el centro de la pantalla
+
+// Indicadores de si las teclas derecha/izquierda están presionadas
+var clickDerecha = false;
+var clickIzquierda = false;
+
+// Matriz con los ladrillos
+var ladrillos = [];
+
+// Puntuación y ladrillos destruidos
+var puntuacion = 0;
+var ladrillosDestruidos = 0;
+
+/**
+ * FUNCIONES
+ */
+
+/**
+ * Función que obtiene un valor al azar entre dos números, 'min' y 'max'.
+ *
+ * Se utiliza para calcular un valor aleatorio de la velocidadY de la pelota.
+ *
+ * @param {number} min El número obtenido no será menor que este valor.
+ * @param {number} max El número obtenido nunca superará este valor.
+ * @return {number}
+ */
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+/**
+ * Inicializa la matriz de ladrillos.
+ */
+function inicializaLadrillos() {
+    ladrillos = [];
+
+    for (col = 0; col < numColumnas; col++) {
+        ladrillos[col] = [];
+        for (fila = 0; fila < numFilas; fila++) {
+            ladrillos[col][fila] = { x: 0, y: 0, visible: true };
+        }
+    }
+}
+
+/**
+ * Actualiza las variables de posición y movimiento de los elementos a sus valores iniciales.
+ */
+function inicializaJuego() {
+    // Posición de la pelota
+    pelotaX = pantalla.width / 2;
+    pelotaY = pantalla.height - (altoRaqueta + radioPelota); // La posición inicial es en el centro de la raqueta
+
+    // Velocidad de la pelota. Se calcula de manera aleatoria para cada partida.
+    velocidadPelotaX = getRandomInt(velocidadMinima, velocidadMaxima);
+    velocidadPelotaY = -getRandomInt(velocidadMinima, velocidadMaxima);
+
+    // Posición de la raqueta
+    raquetaX = (pantalla.width - anchoRaqueta) / 2; // La posición inicial es en el centro de la pantalla
+
+    // Indicadores de si las teclas derecha/izquierda están presionadas
+    clickDerecha = false;
+    clickIzquierda = false;
+
+    // Inicializamos la matriz de ladrillos
+    inicializaLadrillos();
+
+    // Inicializamos la puntuación
+    puntuacion = 0;
+    ladrillosDestruidos = 0;
+}
+
+/**
+ * Dibuja la pelota en las coordenadas (x,y) de la pantalla.
+ *
+ * @param {number} x
+ * @param {number} y
+ */
+function dibujaPelota(x, y) {
     ctx.beginPath();
-    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-    ctx.fillStyle = ballColor;
+    ctx.arc(x, y, radioPelota, 0, Math.PI*2);
+    ctx.fillStyle = colorPelota;
     ctx.fill();
     ctx.closePath();
 }
 
-function drawPaddle(x) {
+/**
+ * Dibuja la raqueta en la coordenada x de la pantalla.
+ *
+ * @param {number} x
+ */
+function dibujaRaqueta(x) {
     ctx.beginPath();
-    ctx.rect(x, canvas.height - paddleHeight, paddleWidth, paddleHeight);
-    ctx.fillStyle = paddleColor;
+    ctx.rect(x, pantalla.height - altoRaqueta, anchoRaqueta, altoRaqueta);
+    ctx.fillStyle = colorRaqueta;
     ctx.fill();
     ctx.closePath();
 }
+
 /**
- * This is the main function.
+ * Dibuja los ladrillos.
+ */
+function dibujaLadrillos() {
+    for (col = 0; col < numColumnas; col++) {
+        for (fila = 0; fila < numFilas; fila++) {
+            if (ladrillos[col][fila].visible) {
+                // Calculamos la posición del ladrillo
+                var ladrilloX = col * (anchoLadrillo + paddingLadrillo) + offsetIzqLadrillo;
+                var ladrilloY = fila * (altoLadrillo + paddingLadrillo) + offsetTopLadrillo;
+
+                // Guardamos las coordenadas del ladrillo en la matriz
+                ladrillos[col][fila].x = ladrilloX;
+                ladrillos[col][fila].y = ladrilloY;
+
+                // Dibujamos el ladrillo
+                ctx.beginPath();
+                ctx.rect(ladrillos[col][fila].x, ladrillos[col][fila].y, anchoLadrillo, altoLadrillo);
+                ctx.fillStyle = colorLadrillo;
+                ctx.fill();
+                ctx.closePath();
+            }
+        }
+    }
+}
+
+function muestraPuntuacion() {
+    ctx.font = "16px Retro Gaming"
+    ctx.fillStyle = colorTexto;
+    ctx.fillText("Puntuación: " + puntuacion, puntuacionX, puntuacionY);
+}
+/**
+ * Esta función detecta si la pelota ha colisionado con algún ladrillo.
  *
- * Delete the canvas.
- * Draw the ball at (x,y) coordinates.
- * Draw the palette at
+ * Para comprobarlo se recorre la matriz de ladrillos y se verifica si el centro (x,y) de la pelota está "dentro"
+ * de alguno de los ladrillos que está visible en estos momentos.
+ *
+ * Esta no es la forma más elegante de hacerlo, pero este juego se está programando con fines de aprendizaje y esta
+ * es la manera más sencilla de hacerlo.
+ *
+ * @return {(boolean|number)[]} true: ha colisionado con un ladrillo / false: no hay colisión con ningún ladrillo.
+ *                              (x,y): Coordenadas del ladrillo colisionado
+ */
+function deteccionColision() {
+    var ladrilloColision = [false, 0, 0];
+
+    for (col = 0; col < numColumnas; col++) {
+        for (fila = 0; fila < numFilas; fila++) {
+            var ladrillo = ladrillos[col][fila];
+
+            if (ladrillo.visible) {
+                if (pelotaX > ladrillo.x && pelotaX < ladrillo.x + anchoLadrillo &&
+                    pelotaY > ladrillo.y && pelotaY < ladrillo.y + altoLadrillo) {
+                        ladrilloColision = [true, col, fila];
+                        break;
+                }
+            }
+        }
+    }
+    return ladrilloColision;
+}
+
+/**
+ * Manejador de eventos cuando se presiona una tecla.
+ *
+ * Modifica el estado de los indicadores de las teclas derecha/izquierda presionadas.
+ * @param {Event} ev
+ */
+function keyDownHandler(ev) {
+    if (ev.keyCode == leftKeyCode) clickIzquierda = true;
+    else if (ev.keyCode == rightKeyCode) clickDerecha = true;
+}
+
+/**
+ * Manejador de eventos cuando se suelta una tecla.
+ *
+ * Modifica el estado de los indicadores de las teclas derecha/izquierda presionadas.
+ * @param {Event} ev
+ */
+function keyUpHandler(ev) {
+    if (ev.keyCode == leftKeyCode) clickIzquierda = false;
+    else if (ev.keyCode == rightKeyCode) clickDerecha = false;
+}
+
+/**
+ * Esta es la rutina principal del juego. Se ejecuta ciclicamenta cada 'ms' milisegundos.
+ *
+ * Borra el contenido de la pantalla.
+ * Dibula los ladrillos visibles actualmente.
+ * Dibuja la pelota en la posición actual.
+ * Dibuja la raqueta en su posición actual.
+ * Muestra la puntuación actual
+ * Detecta si hay colisión de la pelota con algún ladrillo y, si es así, borra el ladrillo, modifica la dirección de la
+ * velocidad de la pelota, suma la puntuación y validamos si hemos ganado (se han destruido todos los ladrillos).
+ * Detecta si hay colisión de la pelota con los bordes y calcula la nueva dirección de la velocidad.
+ * Calcula la nueva posición de la pelota.
+ * Calcula la nueva posición de la raqueta.
  */
 function main() {
-    // Delete canvas and draw all elements
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawBall(x, y);
-    drawPaddle(paddleX);
+    // Borra el contenido de la pantalla y dibuja todos los elementos en su posición actual
+    ctx.clearRect(0, 0, pantalla.width, pantalla.height);
+    dibujaLadrillos();
+    dibujaPelota(pelotaX, pelotaY);
+    dibujaRaqueta(raquetaX);
+    muestraPuntuacion();
 
-    // Detect the ball collision against the walls and bounce
-    if (x+dx < ballRadius || x+dx > canvas.width-ballRadius) dx = -dx;
-    if (y+dy < ballRadius || y+dy > canvas.height-ballRadius) dy = -dy;
+    // Detectamos si hay colisión de la pelota con algún ladrillo
+    colision = deteccionColision();
+    if (colision[0]) {
+        velocidadPelotaY = -velocidadPelotaY;
+        ladrillos[colision[1]][colision[2]].visible = false;
+        ladrillosDestruidos++;
+        puntuacion += (3-colision[2]);
+        if (ladrillosDestruidos == totalLadrillos) {
+            muestraPuntuacion();
+            alert("HAS GANADO!!");
+            inicializaJuego();
+            document.location.reload();
+        }
+    }
 
-    // Update ball coordinate values
-    x += dx;
-    y += dy;
+    // Detecta la colisión de la pelota con los bordes y cambia la dirección de su velocidad
+    // Si la pelota alcanza el borde inferior y no está en contacto con la raqueta, termina el juego
+    if (pelotaX+velocidadPelotaX < radioPelota || pelotaX+velocidadPelotaX > pantalla.width-radioPelota) velocidadPelotaX = -velocidadPelotaX;
+    if (pelotaY+velocidadPelotaY < radioPelota) {
+        velocidadPelotaY = -velocidadPelotaY;
+    }
+    else if (pelotaY+velocidadPelotaY > pantalla.height-radioPelota) { // Comprueba si la pelota ha llegado al borde inferior
+        if (pelotaX > raquetaX && pelotaX < (raquetaX + anchoRaqueta)) { // Comprueba si la pelota ha rebotado con la raqueta
+            velocidadPelotaY = -velocidadPelotaY;
+        }
+        else { // La pelota ha salido por el borde inferior de la pantalla --> GAME OVER
+            alert("GAME OVER!");
+            inicializaJuego();
+            document.location.reload();
+        }
+    }
 
-    // Palette movement
-    if(rightPressed && paddleX < canvas.width-paddleWidth) paddleX += paddleDx;
-    else if(leftPressed && paddleX > 0) paddleX -= paddleDx;
+    // Calcula la nueva posición de la pelota
+    pelotaX += velocidadPelotaX;
+    pelotaY += velocidadPelotaY;
+
+    // Calcula la nueva posición de la raqueta
+    if(clickDerecha && raquetaX < pantalla.width-anchoRaqueta) raquetaX += velocidadRaquetaX;
+    else if(clickIzquierda && raquetaX > 0) raquetaX -= velocidadRaquetaX;
 }
 
-function keyDownHandler(ev) {
-    if (ev.keyCode == leftKeyCode) leftPressed = true;
-    else if (ev.keyCode == rightKeyCode) rightPressed = true;
-}
-
-function keyUpHandler(ev) {
-    if (ev.keyCode == leftKeyCode) leftPressed = false;
-    else if (ev.keyCode == rightKeyCode) rightPressed = false;
-}
-
-// Add Event Listeners
+// Añadimos los manejadores de eventos para detectar la presión sobre las teclas derecha/izquierda
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 
-// Execute de main function every 20 ms
-setInterval(main, 20);
+// Inicializamos las variables del juego
+inicializaJuego();
+
+// Ejecutamos la rutina principal del juego cada 'ms' milisegundos
+setInterval(main, ms);
